3215049df202363d2c613eec7b10ffc3218aca41
Byeong Kwon Kwak <cs@allaboutapps.net>
2016-10-27 14:24:22 +0900

    preferredLanguages[0] returns ru-RU rather than ru only.

--- NSDate+TimeAgo.m
+++ NSDate+TimeAgo.m
@@ -353,7 +353,7 @@ NSLocalizedStringFromTableInBundle(key, @"NSDateTimeAgo", [NSBundle bundleWithPa
  */
 -(NSString *)getLocaleFormatUnderscoresWithValue:(double)value
 {
-    NSString *localeCode = [[NSLocale preferredLanguages] objectAtIndex:0];
+	NSString *localeCode = [[NSLocale currentLocale] objectForKey:NSLocaleLanguageCode];
     
     // Russian (ru)
     if([localeCode isEqual:@"ru"]) {
